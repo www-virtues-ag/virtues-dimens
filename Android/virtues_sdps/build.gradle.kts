d73716dd78c6b1be0632788af43e1736279e39af
Jean Carlo Bodenberg <jean.carlo@virtues.ag>
2026-03-25 08:58:48 -0300

    Remove Maven publishing setup from build.gradle.kts
    Removed Maven publishing configuration and related tasks.

--- Android/virtues_sdps/build.gradle.kts
+++ Android/virtues_sdps/build.gradle.kts
@@ -1,4 +1,3 @@
-import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -8,11 +7,6 @@ plugins {
     alias(libs.plugins.vanniktech.maven.publish)
     alias(libs.plugins.dokka.jetbrains)
 }
-
-val isJitPack = System.getenv("JITPACK") == "true"
-        || System.getenv("jitpack") == "true"
-        || System.getenv("CI") == "true"
-        || System.getenv("ci") == "true"
 
 tasks.dokkaHtml.configure {
     outputDirectory.set(rootProject.layout.projectDirectory.dir("DOCS/SDPS/HTML"))
@@ -24,72 +18,6 @@ tasks.dokkaGfm.configure {
 
 tasks.dokkaJavadoc.configure {
     outputDirectory.set(rootProject.layout.projectDirectory.dir("DOCS/SDPS/JAVADOC"))
-}
-
-mavenPublishing {
-    coordinates("ag.virtues.dimens", "virtues-sdps", "1.0.0")
-
-    configure(
-        AndroidSingleVariantLibrary(
-            publishJavadocJar = true,
-            sourcesJar = true
-        )
-    )
-
-    pom {
-        name.set("AppDimens SDP, HDP, WDP: Scalable Width and Height Dimensions")
-        description.set(
-            "An extension of AppDimens that implements the SDP (Scaled Density Pixels) standard for widths and heights. It automatically generates pre-calculated @dimen resources, ensuring that layout dimensions (Dp) scale consistently across different screen sizes. Perfect for direct use in traditional XML layouts and Composables. " +
-                    "android, kotlin, java, jetpack-compose, xml, swift, swiftui, ios, dp, sp, sdp, ssp, dimensions, responsive, layout, design-system, adaptive, dynamic, fixed, view-system"
-        )
-        url.set("https://github.com/www-virtues-ag/virtues-dimens-dimens")
-        inceptionYear.set("2025")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        developers {
-            developer {
-                id.set("bodenberg")
-                name.set("Jean Bodenberg")
-                email.set("jean.bodenberg@gmail.com")
-            }
-        }
-        scm {
-            connection.set("scm:git:github.com/www-virtues-ag/virtues-dimens-dimens.git")
-            developerConnection.set("scm:git:ssh://github.com/www-virtues-ag/virtues-dimens-dimens.git")
-            url.set("https://github.com/www-virtues-ag/virtues-dimens-dimens")
-        }
-    }
-    
-if (!isJitPack) {
-        signAllPublications()
-        publishToMavenCentral()
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "SonaType"
-            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
-            credentials {
-                username = project.findProperty("mavenCentralUsername") as String?
-                password = project.findProperty("mavenCentralPassword") as String?
-            }
-        }
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/www-virtues-ag/virtues-dimens-dimens")
-            credentials {
-                username = project.findProperty("gpr.user") as String?
-                password = project.findProperty("gpr.key") as String?
-            }
-        }
-    }
 }
 
 android {
