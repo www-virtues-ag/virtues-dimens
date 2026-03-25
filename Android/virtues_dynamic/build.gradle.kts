3a8ab76745eed825a3e7c0b91fca5bdd66722980
Jean Carlo Bodenberg <jean.carlo@virtues.ag>
2026-03-25 09:02:39 -0300

    Remove Maven publishing setup from build.gradle.kts
    Removed Maven publishing configuration and related tasks.

--- Android/virtues_dynamic/build.gradle.kts
+++ Android/virtues_dynamic/build.gradle.kts
@@ -1,18 +1,11 @@
-import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.vanniktech.maven.publish)
     alias(libs.plugins.dokka.jetbrains)
 }
-
-val isJitPack = System.getenv("JITPACK") == "true"
-        || System.getenv("jitpack") == "true"
-        || System.getenv("CI") == "true"
-        || System.getenv("ci") == "true"
 
 tasks.dokkaHtml.configure {
     outputDirectory.set(rootProject.layout.projectDirectory.dir("DOCS/DYNAMIC/HTML"))
@@ -24,72 +17,6 @@ tasks.dokkaGfm.configure {
 
 tasks.dokkaJavadoc.configure {
     outputDirectory.set(rootProject.layout.projectDirectory.dir("DOCS/DYNAMIC/JAVADOC"))
-}
-
-mavenPublishing {
-    coordinates("ag.virtues.dimens", "virtues-dynamic", "1.0.0")
-
-    configure(
-        AndroidSingleVariantLibrary(
-            publishJavadocJar = true,
-            sourcesJar = true
-        )
-    )
-
-    pom {
-        name.set("Virtues Core: Dynamic (DY) and Fixed (FX) Dimensioning")
-        description.set(
-            "The essential Virtues module for responsive scaling via code. It includes the Dynamic (DY) model for proportional dimensioning (ideal for containers and fluid layouts) and the refined Fixed (FX) model for logarithmic scaling (ideal for paddings, margins, and buttons). Supports Jetpack Compose, XML Views (via Data Binding), and direct calls in Kotlin/Java. " +
-                    "(android, kotlin, java, jetpack-compose, xml, swift, swiftui, ios, dp, sp, sdp, ssp, dimensions, responsive, layout, design-system, adaptive, dynamic, fixed, view-system)"
-        )
-        url.set("https://github.com/www-virtues-ag/virtues-dimens-dimens")
-        inceptionYear.set("2025")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        developers {
-            developer {
-                id.set("bodenberg")
-                name.set("Jean Bodenberg")
-                email.set("jean.bodenberg@gmail.com")
-            }
-        }
-        scm {
-            connection.set("scm:git:github.com/www-virtues-ag/virtues-dimens-dimens.git")
-            developerConnection.set("scm:git:ssh://github.com/www-virtues-ag/virtues-dimens-dimens.git")
-            url.set("https://github.com/www-virtues-ag/virtues-dimens-dimens")
-        }
-    }
-    
-if (!isJitPack) {
-        signAllPublications()
-        publishToMavenCentral()
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "SonaType"
-            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
-            credentials {
-                username = project.findProperty("mavenCentralUsername") as String?
-                password = project.findProperty("mavenCentralPassword") as String?
-            }
-        }
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/www-virtues-ag/virtues-dimens-dimens")
-            credentials {
-                username = project.findProperty("gpr.user") as String?
-                password = project.findProperty("gpr.key") as String?
-            }
-        }
-    }
 }
 
 android {
